2eb7526252ddc2be4d92aa353b53ae7d2183ea10
nghtcrwlr444 <yortegavigo@gmail.com>
2026-09-14 19:39:12 -0500

    Refactor selection sort implementation in pract.py
    sssssss

--- pract.py
+++ pract.py
@@ -1,13 +1,13 @@
 def metodo_seleccion(lista) -> list:
-    num = len(lista) 
+    num = len(lista)
     
-    for i in range(num): 
-        min_index = i
-        for j in range(i + 1, num): 
-            if lista[j] < lista[min_index]:
-                min_index = j
-        if min_index != i:
-            lista[i], lista[min_index] = lista[min_index], lista[i]
+    for val_min in range(num):
+        min_index = val_min
+        for val_max in range(val_min + 1, num):
+            if lista[val_max] < lista[min_index]:
+                min_index = val_max
+        if min_index != val_min:
+            lista[val_min], lista[min_index] = lista[min_index], lista[val_min]
     return lista
 
 lista = [2, 8, 5, 3, 9, 4, 1]
