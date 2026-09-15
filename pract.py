205627d64cca3dfea37ffffb96edda4b5e85fa69
nghtcrwlr444 <yortegavigo@gmail.com>
2026-09-14 19:42:34 -0500

    Refactor selection sort method with new variable names
    yup

--- pract.py
+++ pract.py
@@ -1,13 +1,12 @@
 def metodo_seleccion(lista) -> list:
     num = len(lista)
-    
-    for val_min in range(num):
-        min_index = val_min
-        for val_max in range(val_min + 1, num):
-            if lista[val_max] < lista[min_index]:
-                min_index = val_max
-        if min_index != val_min:
-            lista[val_min], lista[min_index] = lista[min_index], lista[val_min]
+    for mano_izq in range(num):
+        min_index = mano_izq
+        for mano_der in range(mano_izq + 1, num):
+            if lista[mano_der] < lista[min_index]:
+                min_index = mano_der
+        if min_index != mano_izq:
+            lista[mano_izq], lista[min_index] = lista[min_index], lista[mano_izq]
     return lista
 
 lista = [2, 8, 5, 3, 9, 4, 1]
